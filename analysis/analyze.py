d6e088a814149ca3affdb843798d77e90aff2144
mcaos <matteo.busi42@gmail.com>
2018-09-20 19:08:29 +0200

    All plots added. Fixed README

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -2,9 +2,30 @@ import pandas
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import rcParams
 import seaborn as sns
 import sys
 import os
+font = {'family' : 'serif',
+        'weight' : 'bold',
+        'size'   : 15}
+        
+plt.rc('text', usetex=True)
+plt.rc('font', **font)
+rcParams.update({'figure.autolayout': True})
+
+def tabulate(res, interesting_pairs):
+    for (depth, fvc) in interesting_pairs:
+        id_res_orig = res[(res["transf"] == "id") & (res["name"] == "orig") & (res["depth"] == depth) & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
+        id_res_einc = res[(res["transf"] == "id") & (res["name"] == "einc") & (res["depth"] == depth) & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
+
+        if len(id_res_orig.index) == 1:
+            orig_r, einc_r = id_res_orig.iloc[0]["rate"], id_res_einc.iloc[0]["rate"]
+            ratio = einc_r / orig_r
+            if fvc.bit_length() == 1 :
+                print("${}$ & $1$ & ${:.2f}$ & ${:.2f}$ & ${:.2f}$\\\\".format(depth, orig_r, einc_r, ratio))
+            else:
+                print("${}$ & $2^{{{}}}$ & ${:.2f}$ & ${:.2f}$ & ${:.2f}$\\\\".format(depth, fvc.bit_length() - 1, orig_r, einc_r, ratio))
 
 def plot_on_pdf (filename, res, transf):
     res_1 = res[(res["transf"] == transf)]
@@ -17,18 +38,22 @@ def plot_on_pdf (filename, res, transf):
                 add_res_orig = res_3[res_3["name"] == "orig"].drop(["transf", "name", "depth", "fvc"], axis=1).groupby(["inv_depth"]).mean().reset_index()
                 add_res_inc  = res_3[res_3["name"] == "inc" ].drop(["transf", "name", "depth", "fvc"], axis=1).groupby(["inv_depth"]).mean().reset_index()
 
-                add_res_orig["diff_sz"] = depth - add_res_orig["inv_depth"]
-                add_res_inc["diff_sz"] = depth - add_res_inc["inv_depth"]
+                add_res_orig["diffsz"] = depth - add_res_orig["inv_depth"]
+                add_res_inc["diffsz"] = depth - add_res_inc["inv_depth"]
 
                 add_res_orig = add_res_orig.drop(["inv_depth"], axis=1)
                 add_res_inc = add_res_inc.drop(["inv_depth"], axis=1)
 
                 # plt.title("transf = {}; depth = {}; fv_c = {}".format(transf, depth, fvc))
-                plt.xticks(np.arange(min(add_res_orig["diff_sz"]) - 1, max(add_res_orig["diff_sz"]) + 1, 1.0))
-
-                add_res_orig.plot(x="diff_sz", y="rate", ax=ax, label="std", marker='+', color='blue', linewidth=2, linestyle='dashed') # BLUE
-                add_res_inc.plot(x="diff_sz", y="rate", ax=ax, label="inc", marker='o', color='orange', linewidth=2) # ORANGE
+                add_res_orig.plot(x="diffsz", y="rate", ax=ax, label="std", marker='+', color='blue', linewidth=2, linestyle='dashed') # BLUE
+                add_res_inc.plot(x="diffsz", y="rate", ax=ax, label="inc", marker='o', color='orange', linewidth=2) # ORANGE
                 
+                plt.xticks(np.arange(min(add_res_orig["diffsz"]), max(add_res_orig["diffsz"]) + 1, 1.0))
+                if depth == 12 and (fvc==512 or fvc==2048):
+                    plt.yticks(np.arange(0, 8000, 1000))
+                plt.xlabel("$\log_2 (\mathit{\# modified\_nodes} + 1)$")
+                plt.ylabel("re-typings/$s$")
+
                 pdf.savefig()
                 plt.close()
 
@@ -37,30 +62,31 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         print(("Usage: {} input_file.csv dest_path\n").format(sys.argv[0]))
     else:
-        res = pandas.read_csv(sys.argv[1], sep=", ").drop(["repeat", "time"], axis=1)
+        res = pandas.read_csv(sys.argv[1], sep=", ", engine="python").drop(["repeat", "time"], axis=1)
 
-        interesting = [(16, 1), (8, 128), (10, 512), (12, 2048), (14, 8192), (16, 32768)]
         # Just plot for "big" enough trees
         res = res[res["depth"] >= 8]
 
         # Create the directory
         os.makedirs("{}".format(sys.argv[2]), exist_ok=True)
-        # Plot results for transf=id; connect by name; with x axis=depth; y axis=rate; 
-        for fvc in res['fvc'].unique():
-            with PdfPages('{}/res_id_{}.pdf'.format(sys.argv[2], fvc)) as pdf:
-                fig, ax = plt.subplots()
-                id_res_orig = res[(res["transf"] == "id") & (res["name"] == "orig") & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
-                id_res_inc = res[(res["transf"] == "id") & (res["name"] == "inc") & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
-                id_res_einc = res[(res["transf"] == "id") & (res["name"] == "einc") & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
-                plt.title("transf = id; fv_c = {}".format(fvc))
+        # # Plot results for transf=id; connect by name; with x axis=depth; y axis=rate; 
+        # for fvc in res['fvc'].unique():
+        #     with PdfPages('{}/res_id_{}.pdf'.format(sys.argv[2], fvc)) as pdf:
+        #         fig, ax = plt.subplots()
+        #         id_res_orig = res[(res["transf"] == "id") & (res["name"] == "orig") & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
+        #         id_res_inc = res[(res["transf"] == "id") & (res["name"] == "inc") & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
+        #         id_res_einc = res[(res["transf"] == "id") & (res["name"] == "einc") & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
+        #         #plt.title("transf = id; fv_c = {}".format(fvc))
 
-                plt.xticks(np.arange(min(id_res_orig["depth"]) - 1, max(id_res_orig["depth"]) + 1, 1.0))
-                #print(np.arange(min(id_res_orig["depth"]) - 1, max(id_res_orig["depth"]) + 1, 1.0))
-                op = id_res_orig.plot(x="depth", y="rate", ax=ax, label="std", marker='+', color='blue', linewidth=2, linestyle='dashed') # BLUE
-                ep = id_res_einc.plot(x="depth", y="rate", ax=ax, label="einc", marker='o', color='orange', linewidth=2) # ORANGE
-                pdf.savefig()
-                plt.close()
+        #         plt.xticks(np.arange(min(id_res_orig["depth"]) - 1, max(id_res_orig["depth"]) + 1, 1.0))
+        #         #print(np.arange(min(id_res_orig["depth"]) - 1, max(id_res_orig["depth"]) + 1, 1.0))
+        #         op = id_res_orig.plot(x="depth", y="rate", ax=ax, label="std", marker='+', color='blue', linewidth=2, linestyle='dashed') # BLUE
+        #         ep = id_res_einc.plot(x="depth", y="rate", ax=ax, label="einc", marker='o', color='orange', linewidth=2) # ORANGE
+        #         pdf.savefig()
+        #         plt.close()
         
+        tabulate(res, [(16,1), (16, 2**7), (16, 2**9), (16, 2**11), (16, 2**13), (16, 2**15)])
+        tabulate(res, [(14,1), (14, 2**7), (14, 2**9), (14, 2**11), (14, 2**13)])
+        tabulate(res, [(12,1), (12, 2**7), (12, 2**9), (12, 2**11)])
+
         plot_on_pdf("{}/".format(sys.argv[2]) + "res_{}_{}_{}.pdf", res, "mod")
-        #plot_on_pdf("plot/{}/res_move.pdf".format(sys.argv[2]), res, "move")
-        #plot_on_pdf("plot/{}/res_elim.pdf".format(sys.argv[2]), res, "elim")
