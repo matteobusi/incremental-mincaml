b57468057a4adbcbbad599cf796c2e072f426c0e
Matteo Busi <matteo.busi@di.unipi.it>
2020-03-20 23:51:55 +0100

    Moved to Core_bench

--- incrementalizer/analysis/analyze.py
+++ incrementalizer/analysis/analyze.py
@@ -6,51 +6,66 @@ from matplotlib import rcParams
 import seaborn as sns
 import sys
 import os
+
+## Names of the experiments, taken from ocaml source
+orig_n = "orig"
+einc_n = "einc"
+finc_n = "finc"
+setupinc_n = "setup+inc"
+setup_n = "setup"
+inc_n = "inc"
+##
+
 font = {'family' : 'serif',
         'weight' : 'bold',
         'size'   : 15}
-        
+
 plt.rc('text', usetex=True)
 plt.rc('font', **font)
 rcParams.update({'figure.autolayout': True})
 
 def tabulate(res, interesting_pairs):
     for (depth, fvc) in interesting_pairs:
-        id_res_orig = res[(res["transf"] == "id") & (res["name"] == "orig") & (res["depth"] == depth) & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
-        id_res_einc = res[(res["transf"] == "id") & (res["name"] == "einc") & (res["depth"] == depth) & (res["fvc"] == fvc)].drop(["transf", "inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
+        id_res_orig = res[(res["name"] == orig_n) & (res["depth"] == depth) & (res["fvc"] == fvc)].drop(["inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
+        id_res_einc = res[(res["name"] == einc_n) & (res["depth"] == depth) & (res["fvc"] == fvc)].drop(["inv_depth", "name", "fvc"], axis=1).groupby("depth").mean().reset_index()
 
         if len(id_res_orig.index) == 1:
             orig_r, einc_r = id_res_orig.iloc[0]["rate"], id_res_einc.iloc[0]["rate"]
             ratio = einc_r / orig_r
-            if fvc.bit_length() == 1 :
+            if fvc.bit_length() == 1:
                 print("${}$ & $1$ & ${:.2f}$ & ${:.2f}$ & ${:.2f}$\\\\".format(depth, orig_r, einc_r, ratio))
             else:
                 print("${}$ & $2^{{{}}}$ & ${:.2f}$ & ${:.2f}$ & ${:.2f}$\\\\".format(depth, fvc.bit_length() - 1, orig_r, einc_r, ratio))
 
-def plot_on_pdf (filename, res, transf):
-    res_1 = res[(res["transf"] == transf)]
-    for depth in res_1["depth"].unique():
-        res_2 = res_1[res_1["depth"] == depth]
+def plot_on_pdf (filename, res):
+    for depth in res["depth"].unique():
+        res_2 = res[res["depth"] == depth]
         for fvc in res_2["fvc"].unique():
             res_3 = res_2[res_2["fvc"] == fvc]
-            with PdfPages(filename.format(transf, depth, fvc)) as pdf:
+            with PdfPages(filename.format(depth, fvc)) as pdf:
                 fig, ax = plt.subplots()
-                add_res_orig = res_3[res_3["name"] == "orig"].drop(["transf", "name", "depth", "fvc"], axis=1).groupby(["inv_depth"]).mean().reset_index()
-                add_res_inc  = res_3[res_3["name"] == "inc" ].drop(["transf", "name", "depth", "fvc"], axis=1).groupby(["inv_depth"]).mean().reset_index()
+                add_res_orig = res_3[res_3["name"] == orig_n].drop(["name", "depth", "fvc"], axis=1).groupby(["inv_depth"]).mean().reset_index()
+                add_res_inc  = res_3[res_3["name"].str.startswith(inc_n)].drop(["name", "depth", "fvc"], axis=1).groupby(["inv_depth"]).mean().reset_index()
 
-                add_res_orig["diffsz"] = depth - add_res_orig["inv_depth"]
+                # add_res_orig["diffsz"] = depth - add_res_orig["inv_depth"]
                 add_res_inc["diffsz"] = depth - add_res_inc["inv_depth"]
 
                 add_res_orig = add_res_orig.drop(["inv_depth"], axis=1)
                 add_res_inc = add_res_inc.drop(["inv_depth"], axis=1)
 
+                # Repeat the row
+                rate = add_res_orig["rate"].values[0]
+                add_res_orig = pandas.DataFrame()
+                for dsz in add_res_inc.drop(["rate"], axis=1).values:
+                    add_res_orig = add_res_orig.append({"diffsz":int(dsz[0]), "rate":rate}, ignore_index=True)
+
                 # plt.title("transf = {}; depth = {}; fv_c = {}".format(transf, depth, fvc))
                 add_res_orig.plot(x="diffsz", y="rate", ax=ax, label="std", marker='+', color='blue', linewidth=2, linestyle='dashed') # BLUE
                 add_res_inc.plot(x="diffsz", y="rate", ax=ax, label="inc", marker='o', color='orange', linewidth=2) # ORANGE
-                
+
                 plt.xticks(np.arange(min(add_res_orig["diffsz"]), max(add_res_orig["diffsz"]) + 1, 1.0))
-                if depth == 12 and (fvc==512 or fvc==2048 or fvc==1024):
-                    plt.yticks(np.arange(0, 8000, 1000))
+                # if depth == 12 and (fvc==512 or fvc==2048 or fvc==1024):
+                #     plt.yticks(np.arange(0, 8000, 1000))
                 plt.xlabel("$\log_2 (\mathit{\# nodes\_diff} + 1)$")
                 plt.ylabel("re-typings/$s$")
 
@@ -62,14 +77,15 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         print(("Usage: {} input_file.csv dest_path\n").format(sys.argv[0]))
     else:
-        res = pandas.read_csv(sys.argv[1], sep=", ", engine="python", dtype={'fvc':int, 'depth':int, 'inv_depth':int, 'rate':float}).drop(["repeat", "time"], axis=1)
+        res = pandas.read_csv(sys.argv[1], sep=", ", engine="python", dtype={'fvc':int, 'depth':int, 'inv_depth':int, 'rate':float})
+        #.drop(["repeat", "time"], axis=1)
 
         # Just plot for "big" enough trees
         res = res[res["depth"] >= 8]
 
         # Create the directory
         os.makedirs("{}".format(sys.argv[2]), exist_ok=True)
-        # # Plot results for transf=id; connect by name; with x axis=depth; y axis=rate; 
+        # # Plot results for transf=id; connect by name; with x axis=depth; y axis=rate;
         # for fvc in res['fvc'].unique():
         #     with PdfPages('{}/res_id_{}.pdf'.format(sys.argv[2], fvc)) as pdf:
         #         fig, ax = plt.subplots()
@@ -84,9 +100,9 @@ if __name__ == "__main__":
         #         ep = id_res_einc.plot(x="depth", y="rate", ax=ax, label="einc", marker='o', color='orange', linewidth=2) # ORANGE
         #         pdf.savefig()
         #         plt.close()
-        
+
         tabulate(res, [(16,1), (16, 2**7), (16, 2**9), (16, 2**11), (16, 2**13), (16, 2**15)])
         tabulate(res, [(14,1), (14, 2**7), (14, 2**9), (14, 2**11), (14, 2**13)])
         tabulate(res, [(12,1), (12, 2**7), (12, 2**9), (12, 2**11)])
 
-        plot_on_pdf("{}/".format(sys.argv[2]) + "res_{}_{}_{}.pdf", res, "mod")
+        plot_on_pdf("{}/".format(sys.argv[2]) + "res_{}_{}.pdf", res)
--- incrementalizer/src/fun/tests/experiments.ml
+++ incrementalizer/src/fun/tests/experiments.ml
@@ -1,9 +1,9 @@
 (* open Core *)
 open Core_bench.Std
-open Batteries
-(* open Benchmark *)
-(* open Benchmark2 *)
 open Core_bench.Simplified_benchmark
+open Core_bench.Simplified_benchmark.Result
+open Core_bench.Simplified_benchmark.Results
+open Batteries
 
 module OriginalFunAlgorithm = Original.TypeAlgorithm(FunSpecification.FunSpecification)
 module IncrementalFunAlgorithm = Incrementalizer.TypeAlgorithm(FunSpecification.FunSpecification)
@@ -12,6 +12,15 @@ open FunSpecification.FunSpecification
 
 open Generator
 open VarSet
+
+(* Names of the experiments *)
+let orig_n = "orig" (* Original typing algorithm *)
+let einc_n = "einc" (* Incremental typing algorithm w. empty initial cache *)
+let finc_n = "finc" (* Incremental typing algorithm w. full initial cache *)
+let setupinc_n = "setup+inc" (* Incremental typing algorithm w. its setup *)
+let setup_n = "setup" (* Just the setup of the above *)
+let inc_n = "inc" (* Just the incremental typing algorithm *)
+
 
 let rec nodecount e = match e with
   | Unit(annot)
@@ -40,44 +49,41 @@ let throughput_original_vs_inc quota verbosity e gamma_init inv_depth_list =
   let measures = Bench.measure
     ~run_config:(Core_bench.Std.Bench.Run_config.create ~quota:quota ~verbosity:verbosity ())
     [
-    Bench.Test.create ~name:"orig"
+      Bench.Test.create ~name:orig_n
         (fun () -> OriginalFunAlgorithm.typing gamma_init e);
-      Bench.Test.create ~name:"incr-fullcache"
-        (
-          let cache = IncrementalFunAlgorithm.get_empty_cache nc in
+      Bench.Test.create ~name:finc_n
+        (let cache = IncrementalFunAlgorithm.get_empty_cache nc in
             ignore (IncrementalFunAlgorithm.build_cache e gamma_init cache);
             fun () -> IncrementalFunAlgorithm.typing cache gamma_init e
         );
-      Bench.Test.create ~name:"incr-emptycache"
+      Bench.Test.create ~name:einc_n
         (fun () -> IncrementalFunAlgorithm.typing (IncrementalFunAlgorithm.get_empty_cache nc) gamma_init e);
-      Bench.Test.create_indexed
-      ~name:"bc+inv+incr"
-      ~args:inv_depth_list
-        (fun d ->
+      Bench.Test.create_indexed ~name:setupinc_n ~args:inv_depth_list
+      (let cache = IncrementalFunAlgorithm.get_empty_cache nc in
+        ignore (IncrementalFunAlgorithm.build_cache e gamma_init cache);
+        fun d ->
           Core.Staged.stage (fun () ->
-          let cache = IncrementalFunAlgorithm.get_empty_cache nc in
-            ignore (IncrementalFunAlgorithm.build_cache e gamma_init cache);
+            (* Enough because the tree isn't actually modified, i.e., after inc. typing the cache will be the same as the initial one! *)
             simulate_modification cache e d;
             IncrementalFunAlgorithm.typing cache gamma_init e
-        ));
-      Bench.Test.create_indexed
-      ~name:"bc+inv"
-      ~args:inv_depth_list
-        (fun d ->
-          Core.Staged.stage(fun () ->
-          let cache = IncrementalFunAlgorithm.get_empty_cache nc in
-            ignore (IncrementalFunAlgorithm.build_cache e gamma_init cache);
-            simulate_modification cache e d;
-            cache
-        ));
+          )
+      );
+      Bench.Test.create_indexed ~name:setup_n ~args:inv_depth_list
+      (let cache = IncrementalFunAlgorithm.get_empty_cache nc in
+        ignore (IncrementalFunAlgorithm.build_cache e gamma_init cache);
+        fun d ->
+          Core.Staged.stage (fun () ->
+            simulate_modification cache e d; cache
+          )
+      );
     ] in
   let results = List.map Bench.analyze measures in
   let results = List.filter_map
     (fun (a : Core_bench.Analysis_result.t Core.Or_error.t) -> match a with
       | Error err -> Printf.printf "Error %s\n%!" (Core.Error.to_string_hum err); None
       | Ok r -> Some r) results in
-  let sexp_res = Core_bench.Simplified_benchmark.to_sexp results in
-    TabRes.print_table (TabRes.table_of_sexp sexp_res)
+  (* Ugly hack, should use extract but it is not exposed by the interface! *)
+  Core_bench.Simplified_benchmark.Results.t_of_sexp (Core_bench.Simplified_benchmark.to_sexp results)
 
 let gen_list min max next =
   let rec gen_aux curr =
@@ -88,24 +94,86 @@ let rec cartesian a b = match b with
 | [] -> []
 | be :: bs -> (List.map (fun ae -> (be, ae)) a) @ (cartesian a bs)
 
+module MinimalResult = struct
+    type t =
+    {
+      name : string;
+      depth : int;
+      fvc : int;
+      inv_depth : int;
+      rate : float
+    }
+end
+
+let split_name name =
+  match String.split_on_char ':' name with
+  | [n] -> (n, -1)
+  | [n; inv_depth] -> (n, Int.of_string inv_depth)
+  | _ -> Printf.eprintf "Name: '%s'\n" name; failwith "Error: does the experiment name contains more than one ':'?"
+
+let extract_minimal (results : Core_bench.Simplified_benchmark.Results.t) depth fvc =
+  (* First look for incr_setup_name and setup_name and merge them *)
+  let rate_of_time t = 1000000000./.t in
+  let project r =
+    MinimalResult.({
+      name = r.full_benchmark_name;
+      depth = depth;
+      fvc = fvc;
+      inv_depth = if (String.starts_with r.full_benchmark_name setup_n) then snd (split_name r.full_benchmark_name) else (-1);
+      rate = rate_of_time r.time_per_run_nanos
+    }) in
+  let to_fix_list = List.filter
+    (fun r -> String.starts_with r.full_benchmark_name setupinc_n)
+    results in
+  let incr_results = List.map
+    (
+      fun sir ->
+        let name, inv_depth = split_name sir.full_benchmark_name in
+        let setup_res = List.find (fun sr -> String.equal sr.full_benchmark_name (setup_n^":"^(String.of_int inv_depth))) results in
+          MinimalResult.({
+            name = inc_n^":"^(String.of_int inv_depth);
+            depth = depth;
+            fvc = fvc;
+            inv_depth = inv_depth;
+            (*
+              TODO: Should use Batteries.Float.max but inexplicably using the Batteries floating points produces a spurious print of '-44604263' on the stdout.
+            *)
+            rate = rate_of_time (sir.time_per_run_nanos -. setup_res.time_per_run_nanos)
+          })
+    ) to_fix_list in
+  (List.map project results) @ incr_results
+
+let print_csv (mr_list : MinimalResult.t list) =
+  List.iter
+  (fun (mr : MinimalResult.t) -> Printf.printf "%s, %d, %d, %d, %f\n" mr.name mr.depth mr.fvc mr.inv_depth mr.rate; flush stdout) mr_list
+
 let _ =
-  if Array.length Sys.argv < 4 then
-    Printf.printf "%s quota min_depth max_depth\n" Sys.argv.(0)
+   if Array.length Sys.argv < 4 then
+    Printf.eprintf "%s quota min_depth max_depth\n" Sys.argv.(0)
   else
-    let quota, min_depth, max_depth = Core_bench.Bench.Quota.of_string Sys.argv.(1), int_of_string Sys.argv.(2), int_of_string Sys.argv.(3) in
+    let quota, min_depth, max_depth =
+      Core_bench.Bench.Quota.of_string Sys.argv.(1), int_of_string Sys.argv.(2), int_of_string Sys.argv.(3) in
     let depth_list = gen_list min_depth max_depth (fun n -> n+2) in  (* Seems that big AST have ~20k nodes, cfr. [Erdweg et al.] *)
     let len = List.length depth_list in
+    Printf.printf "name, depth, fvc, inv_depth, rate\n"; flush stdout;
     List.iteri (fun i depth -> (
-      let fv_c = BatInt.pow 2 (depth-1) in
+      let fv_c_list = 1 :: gen_list (BatInt.pow 2 7) (BatInt.pow 2 (depth-1)) (fun n -> n*2) in
       let inv_depth_list = [1; 2; 3] @ gen_list 4 (depth - 1) (fun n -> n + 2) in
-      Printf.printf "=== BEGIN: [%d/%d] -- depth=%d; fv_c=%d ===\n" (i+1) len depth fv_c;
-      flush stdout;
-      let e = Generator.gen_ibop_ids_ast depth "+" fv_c in
-      let initial_gamma_list e = (List.map (fun id -> (id, TInt)) (VarSet.elements (compute_fv e))) in
-      let gamma_init = (FunContext.add_list (initial_gamma_list e) (FunContext.get_empty_context ()) ) in
-      throughput_original_vs_inc quota Core_bench.Verbosity.Low e gamma_init inv_depth_list;
-      Printf.printf "=== END: [%d/%d] -- depth=%d; fv_c=%d ===\n" (i+1) len depth fv_c;
-      flush stdout))
+      Printf.eprintf "[%d/%d] depth=%d ...\n" (i+1) len depth;
+      flush stderr;
+        List.iteri (fun j fv_c -> (
+          Printf.eprintf "\t[%d/%d] fv_c=%d ..." (j+1) (List.length fv_c_list) fv_c;
+          flush stderr;
+          let e = Generator.gen_ibop_ids_ast depth "+" fv_c in
+          let initial_gamma_list e = (List.map (fun id -> (id, TInt)) (VarSet.elements (compute_fv e))) in
+          let gamma_init = (FunContext.add_list (initial_gamma_list e) (FunContext.get_empty_context ()) ) in
+          let simplified_results = throughput_original_vs_inc quota Core_bench.Verbosity.Quiet e gamma_init inv_depth_list in
+          print_csv (extract_minimal simplified_results depth fv_c);
+          Printf.eprintf "done!\n";
+          flush stderr;
+        )) fv_c_list
+        )
+      )
     depth_list
 
 (*  (* Must use either Std.Hashtbl or Batteries WeakHashtbl for caches! *)
