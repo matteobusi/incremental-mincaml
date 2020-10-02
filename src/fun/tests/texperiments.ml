e2a7c15d5cd7cacbd1037e425d507df5fef85746
Matteo Busi <matteo.busi@di.unipi.it>
2020-10-02 15:25:58 +0200

    Added threshold to texperiments

--- src/fun/tests/texperiments.ml
+++ src/fun/tests/texperiments.ml
@@ -6,13 +6,14 @@ open VarSet
 module IncrementalFunAlgorithm = Incrementalizer.TypeAlgorithm(FunSpecification.FunSpecification)
 
 let _ =
-   if Array.length Sys.argv < 4 then
-    Printf.eprintf "%s quota min_depth max_depth\n" Sys.argv.(0)
+   if Array.length Sys.argv < 5 then
+    Printf.eprintf "%s quota min_depth max_depth threshold_fractions\n" Sys.argv.(0)
   else
-     let quota, min_depth, max_depth =
+     let quota, min_depth, max_depth, threshold_fractions =
       Quota.of_string Sys.argv.(1),
       int_of_string Sys.argv.(2),
-      int_of_string Sys.argv.(3) in
+      int_of_string Sys.argv.(3),
+      int_of_string Sys.argv.(4) in
     let depth_list = Generator.gen_list min_depth max_depth (fun n -> n+2) in  (* Seems that big AST have ~20k nodes, cfr. [Erdweg et al.] *)
     let len = List.length depth_list in
     Printf.printf "name, fvc, invalidation_parameter, nodecount, diffsz, threshold, rate\n"; Out_channel.flush stdout;
@@ -25,17 +26,40 @@ let _ =
           Printf.eprintf "\t[%d/%d] fv_c=%d ...\n" (j+1) (List.length fv_c_list) fv_c;
           Out_channel.flush stderr;
           let e = Generator.ibop_gen_ast depth "+" fv_c in
+          let nc = Generator.nodecount e in
           let initial_gamma_list e = (List.map ~f:(fun id -> (id, TInt)) (VarSet.elements (compute_fv e))) in
+          let t_list = (List.map ~f:(fun v -> Some v) (0::(Generator.gen_list (nc/threshold_fractions) nc (fun s -> s + int_of_float (0.5 +. float_of_int (nc-1)/. float_of_int (threshold_fractions - 1))))))@[None] in
           List.iteri ~f:(fun k inv_depth ->
-            Printf.eprintf "\t\t[%d/%d] inv_depth=%d ... " (k+1) (List.length inv_depth_list) inv_depth;
+            Printf.eprintf "\t\t[%d/%d] inv_depth=%d ... \n" (k+1) (List.length inv_depth_list) inv_depth;
             Out_channel.flush stderr;
             let gamma_init = (FunContext.add_list (initial_gamma_list e) (FunContext.get_empty_context ()) ) in
-            let orig_vs_inc_res = Experiments.throughput_original_vs_inc quota Core_bench.Verbosity.Quiet IncrementalFunAlgorithm.typing Generator.ibop_sim_change inv_depth fv_c gamma_init e in
-            let caches_res = Experiments.throughput_caches quota Core_bench.Verbosity.Quiet IncrementalFunAlgorithm.typing fv_c gamma_init e in
-              Experiments.print_csv orig_vs_inc_res;
-              Experiments.print_csv caches_res;
-              Printf.eprintf "done!\n";
+            List.iteri ~f:(fun l t ->
+              Printf.eprintf "\t\t[%d/%d] threshold=%d ... " (l+1) (List.length t_list) (Option.value t ~default:(-1));
               Out_channel.flush stderr;
+              let orig_vs_inc_res =
+                Experiments.throughput_original_vs_inc
+                  quota
+                  Core_bench.Verbosity.Quiet
+                  ?threshold:t
+                  IncrementalFunAlgorithm.typing
+                  Generator.ibop_sim_change
+                  inv_depth
+                  fv_c
+                  gamma_init
+                  e in
+              let caches_res =
+                Experiments.throughput_caches
+                  quota
+                  Core_bench.Verbosity.Quiet
+                  IncrementalFunAlgorithm.typing
+                  fv_c
+                  gamma_init
+                  e in
+                    Experiments.print_csv orig_vs_inc_res;
+                    Experiments.print_csv caches_res;
+                    Printf.eprintf "done!\n";
+                    Out_channel.flush stderr;
+            ) t_list
           ) inv_depth_list
         )) fv_c_list
         )
